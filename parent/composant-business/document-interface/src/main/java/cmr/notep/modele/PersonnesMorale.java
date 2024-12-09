8c97426578413308f13e2e6a2edbda51d38c08f3
kouamdo <kouamdoux@gmail.com>
2024-12-09 19:07:45 +0300

    Update inheritance classes

--- parent/composant-business/document-interface/src/main/java/cmr/notep/modele/PersonnesMorale.java
+++ parent/composant-business/document-interface/src/main/java/cmr/notep/modele/PersonnesMorale.java
@@ -1,16 +1,17 @@
 package cmr.notep.modele;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class PersonnesMorale extends Personnes{
+@SuperBuilder
+public class PersonnesMorale extends Personnes {
     private String raisonsociale ;
     private String code ;
 }
--- parent/composant-business/document-interface/src/main/java/cmr/notep/modele/PersonnesPhysique.java
+++ parent/composant-business/document-interface/src/main/java/cmr/notep/modele/PersonnesPhysique.java
@@ -3,12 +3,14 @@ package cmr.notep.modele;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@SuperBuilder
 public class PersonnesPhysique extends Personnes {
     private String nom ;
     private String prenom;
