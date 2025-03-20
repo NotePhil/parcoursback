b67c1e900ec0fd87f3a7727ebae252bfadc76b64
kouamdo <kouamdoux@gmail.com>
2025-03-20 07:21:32 +0300

    mise a jour des relations et update du backup

--- parent/composant-business/document-interface/src/main/java/cmr/notep/modele/Distributeurs.java
+++ parent/composant-business/document-interface/src/main/java/cmr/notep/modele/Distributeurs.java
@@ -1,11 +1,9 @@
 package cmr.notep.modele;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-import org.apache.maven.artifact.DefaultArtifact;
 
 import java.util.Date;
 import java.util.List;
