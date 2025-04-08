f3087d43eaf2406f11ab6b6fd5335748e56396ef
kouamdo <kouamdoux@gmail.com>
2025-04-08 22:48:31 +0300

    Mise a jour des fonctionnalité pour les services de login: Action et Utilisateurs

---
+++ parent/composant-business/document-service/src/main/java/cmr/notep/business/ActionsBusiness.java
@@ -0,0 +1,31 @@
+package cmr.notep.business;
+
+import cmr.notep.dao.DaoAccessorService;
+import cmr.notep.modele.Actions;
+import cmr.notep.repository.ActionsRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static cmr.notep.config.DocumentConfig.dozerMapperBean;
+
+@Component
+@Slf4j
+@Transactional
+public class ActionsBusiness {
+
+    private final DaoAccessorService daoAccessorService ;
+
+
+    public ActionsBusiness(DaoAccessorService daoAccessorService) {
+        this.daoAccessorService = daoAccessorService;
+    }
+
+    public List<Actions> avoirToutesActions() {
+        return daoAccessorService.getRepository(ActionsRepository.class).findAll()
+                .stream().map(actions -> dozerMapperBean.map(actions,Actions.class))
+                .collect(Collectors.toList());
+    }
+}
---
+++ parent/composant-business/document-service/src/main/java/cmr/notep/business/UtilisateursBusiness.java
@@ -0,0 +1,34 @@
+package cmr.notep.business;
+
+import cmr.notep.dao.DaoAccessorService;
+import cmr.notep.modele.Attributs;
+import cmr.notep.modele.Utilisateurs;
+import cmr.notep.repository.AttributsRepository;
+import cmr.notep.repository.UtilisateursRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static cmr.notep.config.DocumentConfig.dozerMapperBean;
+
+@Component
+@Slf4j
+@Transactional
+public class UtilisateursBusiness {
+
+    private final DaoAccessorService daoAccessorService ;
+
+    public UtilisateursBusiness(DaoAccessorService daoAccessorService)
+    {
+        this.daoAccessorService = daoAccessorService;
+    }
+
+    public List<Utilisateurs> avoirToutUser() {
+        return daoAccessorService.getRepository(UtilisateursRepository.class).findAll()
+                .stream().map(user -> dozerMapperBean.map(user, Utilisateurs.class))
+                .collect(Collectors.toList());
+    }
+}
