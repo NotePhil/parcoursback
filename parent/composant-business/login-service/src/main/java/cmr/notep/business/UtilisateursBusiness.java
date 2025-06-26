6ab57a4773028e7c37a65c520f760eaa98073a12
kouamdo <kouamdoux@gmail.com>
2025-06-26 12:33:59 +0300

    check call API ,update document and login services

--- parent/composant-business/login-service/src/main/java/cmr/notep/business/UtilisateursBusiness.java
+++ parent/composant-business/login-service/src/main/java/cmr/notep/business/UtilisateursBusiness.java
@@ -10,10 +10,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashMap;
+
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 import static cmr.notep.config.LoginConfig.dozerMapperBean;
