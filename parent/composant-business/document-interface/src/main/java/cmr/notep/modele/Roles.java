3558c4d8a5e096566f7c9a994b5be60dccbfae56
kouamdo <kouamdoux@gmail.com>
2025-09-19 19:32:11 +0300

    fix call Validations.java and role

--- parent/composant-business/document-interface/src/main/java/cmr/notep/modele/Roles.java
+++ parent/composant-business/document-interface/src/main/java/cmr/notep/modele/Roles.java
@@ -1,9 +1,6 @@
 package cmr.notep.modele;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.util.Date;
@@ -12,9 +9,9 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString(exclude = {"personnels", "missions"})
-@EqualsAndHashCode(exclude = {"personnels", "missions"})
-@JsonIgnoreProperties(value = {"personnels", "missions"}, ignoreUnknown = true)
+@ToString(exclude = {"personnels", "missions","validations"})
+@EqualsAndHashCode(exclude = {"personnels", "missions","validations"})
+@JsonIgnoreProperties(value = {"personnels", "missions","validations"}, ignoreUnknown = true)
 @Builder
 public class Roles {
     private String id;
--- parent/composant-business/document-interface/src/main/java/cmr/notep/modele/Validations.java
+++ parent/composant-business/document-interface/src/main/java/cmr/notep/modele/Validations.java
@@ -1,6 +1,5 @@
 package cmr.notep.modele;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 import java.util.Date;
@@ -10,9 +9,6 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@ToString(exclude = {"role"})
-@EqualsAndHashCode(exclude = {"role"})
-@JsonIgnoreProperties(value = {"role"}, ignoreUnknown = true)
 public class Validations {
     private String id;
     private String libelle ;
