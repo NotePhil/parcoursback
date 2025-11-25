85e974e7e91faa06d238efa131698b1dff7c7cd0
Notephil <ngongang_philippe@yahoo.fr>
2025-11-26 00:54:44 +0100

    Update Validations.java
    Correction bug données récursives

--- parent/composant-business/document-interface/src/main/java/cmr/notep/modele/Validations.java
+++ parent/composant-business/document-interface/src/main/java/cmr/notep/modele/Validations.java
@@ -1,5 +1,6 @@
 package cmr.notep.modele;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 import java.util.Date;
@@ -8,6 +9,9 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString(exclude = {"docetats"})
+@EqualsAndHashCode(exclude = {"docetats"})
+@JsonIgnoreProperties(value = {"docetats"}, ignoreUnknown = true)
 @Builder
 public class Validations {
     private String id;
